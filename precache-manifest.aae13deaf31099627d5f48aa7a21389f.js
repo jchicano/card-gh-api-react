43acc936f63b97cc76a911adf24274d8dcccae3a
jchicano <jesus.d.chicano@gmail.com>
2020-08-05 21:59:42 +0200

    Updates

---
+++ precache-manifest.aae13deaf31099627d5f48aa7a21389f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d40406427b347494380e8bcd9d384e",
+    "url": "/card-gh-api-react/index.html"
+  },
+  {
+    "revision": "d7ec1a890420553da087",
+    "url": "/card-gh-api-react/static/css/main.1bad60cd.chunk.css"
+  },
+  {
+    "revision": "92e44c1e7a9057454c23",
+    "url": "/card-gh-api-react/static/js/2.9cc393d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-gh-api-react/static/js/2.9cc393d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ec1a890420553da087",
+    "url": "/card-gh-api-react/static/js/main.b6ad12f7.chunk.js"
+  },
+  {
+    "revision": "daa30dc11ee6bf9ce1d0",
+    "url": "/card-gh-api-react/static/js/runtime-main.fae0a76c.js"
+  }
+]);
